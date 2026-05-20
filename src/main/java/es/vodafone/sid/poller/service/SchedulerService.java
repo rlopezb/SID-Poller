91b8f98068ea784345cda5c167e403813833d905
Rafael López Benavente <rlopezb@gmail.com>
2026-05-20 11:32:41 +0200

    Remove unused Lombok import from SchedulerService for cleaner code

--- src/main/java/es/vodafone/sid/poller/service/SchedulerService.java
+++ src/main/java/es/vodafone/sid/poller/service/SchedulerService.java
@@ -1,6 +1,5 @@
 package es.vodafone.sid.poller.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
