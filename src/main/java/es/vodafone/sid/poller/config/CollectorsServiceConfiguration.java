1df7ad836685852cd18814cf8cdc5c3e41c00971
Rafael López Benavente <rlopezb@gmail.com>
2026-05-21 16:31:57 +0200

    Remove unused Lombok import from CollectorsServiceConfiguration for cleaner code

--- src/main/java/es/vodafone/sid/poller/config/CollectorsServiceConfiguration.java
+++ src/main/java/es/vodafone/sid/poller/config/CollectorsServiceConfiguration.java
@@ -3,13 +3,11 @@ package es.vodafone.sid.poller.config;
 import es.vodafone.sid.poller.collector.SnmpCollector;
 import es.vodafone.sid.poller.collector.SshCollector;
 import es.vodafone.sid.poller.service.CollectorsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class CollectorsServiceConfiguration {
 
   @Bean
