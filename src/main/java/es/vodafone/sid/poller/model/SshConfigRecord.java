eeaaec0e9e8c81585fca04d6fbf549ba5505bae8
Rafael López Benavente <rlopezb@gmail.com>
2026-05-27 17:44:16 +0200

    SSH and SNMP config records

--- src/main/java/es/vodafone/sid/poller/model/SshConfigRecord.java
+++ src/main/java/es/vodafone/sid/poller/model/SshConfigRecord.java
@@ -3,8 +3,7 @@ package es.vodafone.sid.poller.model;
 public record SshConfigRecord(
     short collectorId,
     short elementTypeId,
-    short maxOid,
-    short por,
+    short port,
     String username,
     String password
 ) {
