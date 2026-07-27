626e248438089be09583670b6763140471c308a2
Rafael López Benavente <rlopezb@gmail.com>
2026-07-27 12:26:00 +0200

    Update logging configuration and refactor application structure for SNMP

--- src/main/java/es/vodafone/sid/poller/PollerApplication.java
+++ src/main/java/es/vodafone/sid/poller/PollerApplication.java
@@ -1,5 +1,7 @@
 package es.vodafone.sid.poller;
 
+import org.snmp4j.log.JavaLogFactory;
+import org.snmp4j.log.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class PollerApplication {
+  static {
+    LogFactory.setLogFactory(new JavaLogFactory());
+  }
   public static void main(String[] args) {
     SpringApplication.run(PollerApplication.class, args);
   }
