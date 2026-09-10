664bd0a4cabfa2ae95038fe5f31b25a0ea361a18
rlopezb <rlopezb@gmail.com>
2026-09-10 13:56:25 +0200

    SNMP and SSH workers added

--- src/main/java/es/vodafone/sid/poller/model/Metric.java
+++ src/main/java/es/vodafone/sid/poller/model/Metric.java
@@ -16,6 +16,38 @@ public record Metric(
     Short groupId,
     Short serviceId,
     Short serviceTypeId,
+    Boolean error,
     BigInteger value
 ) {
+  public String map(){
+    if(value == null) return null;
+    StringBuilder builder = new StringBuilder();
+    builder.setLength(0);
+    builder.append("metric");
+    appendTag(builder, "srcId", this.srcId);
+    appendTag(builder, "elementId", this.elementId);
+    appendTag(builder, "elementTypeId", this.elementTypeId);
+    appendTag(builder, "siteId", this.siteId);
+    appendTag(builder, "cdcId", this.cdcId);
+    appendTag(builder, "zoneId", this.zoneId);
+    appendTag(builder, "netId", this.netId);
+    appendTag(builder, "archId", this.archId);
+    appendTag(builder, "groupId", this.groupId);
+    appendTag(builder, "serviceId", this.serviceId);
+    appendTag(builder, "serviceTypeId", this.serviceTypeId);
+    appendTag(builder, "error", this.error);
+    builder.append(' ');
+    appendValue(builder, this.value());
+    builder.append(' ').append(toEpochNanos(this.instant()));
+    return builder.toString();
+  }
+  private void appendTag(StringBuilder builder, String key, Object value) {
+    if (value!=null) builder.append(',').append(key).append('=').append(value);
+  }
+  private void appendValue(StringBuilder builder, BigInteger value) {
+    builder.append("value=").append(value).append('u');
+  }
+  private long toEpochNanos(Instant instant) {
+    return instant.getEpochSecond() * 1_000_000_000L + instant.getNano();
+  }
 }
--- src/main/java/es/vodafone/sid/poller/strategy/BaseSourceType.java
+++ src/main/java/es/vodafone/sid/poller/strategy/BaseSourceType.java
@@ -7,23 +7,21 @@ import java.math.BigInteger;
 import java.time.Instant;
 
 public abstract class BaseSourceType implements SourceType {
+  protected static Metric metric(Source source, Instant instant, BigInteger value) {
+    return new Metric(
+        instant,
+        source.id(), source.elementId(), source.elementTypeId(),
+        source.siteId(), source.cdcId(), source.zoneId(), source.netId(),
+        source.archId(), source.groupId(), source.serviceId(), source.serviceTypeId(),
+        value == null,  value
+    );
+  }
 
-    protected static Metric metric(Source source, Instant instant, BigInteger value) {
-        return new Metric(
-            instant,
-            source.id(), source.elementId(), source.elementTypeId(),
-            source.siteId(), source.cdcId(), source.zoneId(), source.netId(),
-            source.archId(), source.groupId(), source.serviceId(), source.serviceTypeId(),
-            value
-        );
-    }
+  protected static BigInteger parse(String rawValue) {
+    return new BigInteger(rawValue.trim());
+  }
 
-    protected static BigInteger parse(String rawValue) {
-        return new BigInteger(rawValue.trim());
-    }
-
-    public static Metric nullMetric(Source source, Instant instant) {
-        return metric(source, instant, null);
-    }
-
+  public static Metric nullMetric(Source source, Instant instant) {
+    return metric(source, instant, null);
+  }
 }
